ac275e867b67dd522779440a4eda414403a08751
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-12-07 13:03:50 -0700

    upwrap presents

--- components/presents/presents.js
+++ components/presents/presents.js
@@ -4,12 +4,51 @@ import {StyleSheet, Text, View, Image, FlatList} from 'react-native';
 const image = require('../')
 const array = [ image, image, image, image ]
 export default class Presents extends Component{
+
+  state = {
+    firstPresIsUW: false,
+    secondPresIsUW: false,
+    thirdPresIsUW: false,
+    fourthPresIsUW: false,
+  }
+
+  generateRandomPresent = () => {
+    const possiblePresents = []
+  }
+
+  unwrapPresent = (id) => {
+    let presentToUpdate = ''
+    switch(id) {
+      case 1:
+        presentToUpdate = 'firstPresIsUW'
+        break
+      case 2:
+        presentToUpdate = 'secondPresIsUW'
+        code block
+        break
+      case 3:
+        presentToUpdate = 'thirdPresIsUW'
+        code block
+        break
+      case 4:
+        presentToUpdate = 'fourthPresIsUW'
+        code block
+        break
+      default:
+        break
+    }
+    this.setState({
+      ...this.state,
+      [presentToUpdate]: true
+    })
+  }
+
   renderItem = item => {
     return (
       <View>
         <Image
           source={ image }
-
+          onPress={this.unwrapPresent()}
         />
       </View>
     )
